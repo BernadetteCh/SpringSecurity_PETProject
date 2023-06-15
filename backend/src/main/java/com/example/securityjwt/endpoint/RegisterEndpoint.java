2f17a3bf2c5d3af3ef9d861090a8541ef195cdba
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-15 10:23:39 +0200

    Create endpoint class for registration

---
+++ backend/src/main/java/com/example/securityjwt/endpoint/RegisterEndpoint.java
@@ -0,0 +1,23 @@
+package com.example.securityjwt.endpoint;
+
+import com.example.securityjwt.endpoint.dto.RegistrationDTO;
+import com.example.securityjwt.logic.RegisterService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("register")
+public class RegisterEndpoint {
+    private final RegisterService registerService;
+
+    public RegisterEndpoint(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@RequestBody RegistrationDTO registration) {
+        System.out.println(registration.firstName());
+        registerService.register(registration);
+    }
+}
