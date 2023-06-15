aa29dc3041cdea51340f832f4f15b7467ca43998
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-15 10:28:03 +0200

    Change authentication data and edd path of endpoint

--- frontend/security/src/fetchmethods.js
+++ frontend/security/src/fetchmethods.js
@@ -1,25 +1,40 @@
 import { Buffer } from "buffer";
 
 const authentication = {
-  username: "bernadette",
+  firstName: "bernadette",
+  email: "bernadette@gmail.com",
   password: "1234",
 };
 
 const BACKEND_ENDPOINT = "http://localhost:8080";
 const BACKEND_MESSAGES = `${BACKEND_ENDPOINT}/messages`;
 const BACKEND_LOGIN = `${BACKEND_ENDPOINT}/login`;
+const BACKEND_REGISTER = `${BACKEND_ENDPOINT}/register`;
 
 export function handleUnauthorized(setMessage) {
   fetch(BACKEND_MESSAGES + "/unauthorized")
-    .then((response) => console.log(response))
-    .then((text) => console.log(text))
+    .then((response) => response.text())
+    .then((text) => setMessage("You are not authorized to be here"))
     .catch((error) => console.log("ERROR: " + error));
+}
+
+export function handleRegister(setMessage) {
+  const headers = new Headers();
+  //  headers.set("Authorization", "Basic" + authentication);
+  fetch(BACKEND_REGISTER, {
+    method: "POST",
+    headers: headers,
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(authentication),
+  })
+    .then((response) => setMessage(response.status))
+    .catch((error) => console.log(`ERROR` + error));
 }
 
 export function handleAuthorized(setMessage) {
   const headers = new Headers();
   const auth = Buffer.from(
-    authentication.username + ":" + authentication.password
+    authentication.email + ":" + authentication.password
   ).toString("base64");
   headers.set("Authorization", "Basic " + auth);
   return fetch(BACKEND_MESSAGES + "/authorized", {
@@ -31,19 +46,25 @@ export function handleAuthorized(setMessage) {
     .catch((error) => console.log("ERROR: " + error));
 }
 
-export function handleLogin(setMessage) {
+export function handleLogin(setLoginStatus) {
   const headers = new Headers();
   const auth = Buffer.from(
-    authentication.username + ":" + authentication.password
+    authentication.email + ":" + authentication.password
   ).toString("base64");
   headers.set("Authorization", "Basic " + auth);
   return fetch(BACKEND_LOGIN, { method: "GET", headers: headers })
     .then((response) => response.text())
     .then((jwt) => {
-      setMessage("JWT: " + jwt);
+      setLoginStatus(200);
       localStorage.setItem("jwt", jwt);
     })
     .catch((error) => console.log("ERROR: " + error));
+}
+
+export function handleLogout(setLoginStatus, setMessage) {
+  localStorage.clear();
+  setLoginStatus(400);
+  setMessage("You are logged out");
 }
 
 export function handleCustomized(setMessage) {
