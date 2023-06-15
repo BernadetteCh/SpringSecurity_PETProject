74feb60ebbfdb19a4d29b72a1c0320b9047fa34c
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-15 10:24:05 +0200

    Create class and method to register a user

---
+++ backend/src/main/java/com/example/securityjwt/logic/RegisterService.java
@@ -0,0 +1,33 @@
+package com.example.securityjwt.logic;
+
+import com.example.securityjwt.data.User;
+import com.example.securityjwt.data.UserRepository;
+import com.example.securityjwt.endpoint.dto.RegistrationDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Service
+public class RegisterService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public RegisterService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public void register(RegistrationDTO registrationDTO) {
+        System.out.println(registrationDTO.password()+"!!!!!!!!!!!!!!!!!!!!");
+        System.out.println(registrationDTO.firstName() + "!!!!!!!!!!!");
+        String encodedPassword = passwordEncoder.encode(registrationDTO.password());
+        User user = new User();
+        user.setFirstName(registrationDTO.firstName());
+        user.setEmail(registrationDTO.email());
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+    }
+
+}
