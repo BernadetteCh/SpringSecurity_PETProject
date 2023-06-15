df468236db2e6240bdca0e437c4807cc0faff9d0
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-15 10:28:40 +0200

    Import functions for log a user in and out

--- frontend/security/src/App.jsx
+++ frontend/security/src/App.jsx
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import { handleUnauthorized } from "./fetchmethods";
 import { handleAuthorized } from "./fetchmethods";
 import { handleLogin } from "./fetchmethods";
 import { handleCustomized } from "./fetchmethods";
+import { handleRegister } from "./fetchmethods";
+import { handleLogout } from "./fetchmethods";
 
 function App() {
   const [message, setMessage] = useState();
+  const [loginStatus, setLoginStatus] = useState("");
+  console.log(loginStatus);
   return (
     <>
-      <h1>Spring Securitey JWT</h1>
+      <h1>Spring Security JWT</h1>
       <Button
         name={"Unauthorized_Message"}
         onClick={() => handleUnauthorized(setMessage)}
@@ -19,11 +23,20 @@ function App() {
         name={"Authorized_Message"}
         onClick={() => handleAuthorized(setMessage)}
       />
-      <Button name={"Login"} onClick={() => handleLogin(setMessage)} />
+      {loginStatus === 200 ? (
+        <Button
+          name={"Logout"}
+          onClick={() => handleLogout(setLoginStatus, setMessage)}
+        />
+      ) : (
+        <Button name={"Login"} onClick={() => handleLogin(setLoginStatus)} />
+      )}
+
       <Button
         name={"Customized_Message"}
         onClick={() => handleCustomized(setMessage)}
       />
+      <Button name={"Register"} onClick={() => handleRegister(setMessage)} />
       <p>{message}</p>
     </>
   );
