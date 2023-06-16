3bf3749c31950b8ca3fc258fa0e2166e407e6232
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-16 20:54:56 +0200

    No relevant changes

--- backend/src/main/java/com/example/securityjwt/endpoint/MessageEndpoint.java
+++ backend/src/main/java/com/example/securityjwt/endpoint/MessageEndpoint.java
@@ -1,5 +1,6 @@
 package com.example.securityjwt.endpoint;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
