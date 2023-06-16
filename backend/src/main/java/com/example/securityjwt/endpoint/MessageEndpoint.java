3f4a89b8d39ed81f12db09824157d39323b24fe2
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-16 21:09:11 +0200

    Change content of return message

--- backend/src/main/java/com/example/securityjwt/endpoint/MessageEndpoint.java
+++ backend/src/main/java/com/example/securityjwt/endpoint/MessageEndpoint.java
@@ -1,8 +1,11 @@
 package com.example.securityjwt.endpoint;
 
 
+import com.example.securityjwt.data.User;
+import com.example.securityjwt.security.UserPrincipal;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -13,7 +16,7 @@ public class MessageEndpoint {
     @GetMapping("/unauthorized")
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String unauthorized() {
-        return "Hi from endpoint unauthorized";
+        return "This message will never be displayed due to unauthorization";
     }
 
     @GetMapping("/authorized")
@@ -25,7 +28,7 @@ public class MessageEndpoint {
     @GetMapping("/authorized/customized")
     @ResponseStatus(HttpStatus.ACCEPTED)
     String customized(Authentication authentication) {
-        return "Hello, " + authentication.getName();
+        return "Hello, nice to have you here";
     }
 
 }
